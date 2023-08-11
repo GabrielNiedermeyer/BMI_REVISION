e466f377c57daa6afb25413652c48ceddbe3206c
GeovaneJEan <geovane.99@hotmail.com>
2023-08-11 16:49:51 +0100

    resultActivity created Geo

---
+++ app/src/main/java/com/newdev/bmi_revision/ResultActivity.kt
@@ -0,0 +1,39 @@
+package com.newdev.bmi_revision
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.result)
+
+
+        val tvResult = findViewById<TextView>(R.id.textViewResult)
+        val tvClassification = findViewById<TextView>(R.id.textViewClassification)
+        val result = intent.getFloatExtra("EXTRA_RESULT",0.1f)
+
+        tvResult.text = result.toString()
+
+        val classification = if (result < 18.5f){
+            "Under Wheight"
+        }else if(result in 18.5f..24.9f){
+            "Normal Wheight"
+        }else if(result in 25f..29.9f){
+            "Over Wheight"
+            }else if(result in 30f..34.9f){
+                "Obese"
+            }else {
+                "Extremely obese"
+}
+        tvClassification.text = getString(R.string.MessageClassification,classification)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
+    }
+}
